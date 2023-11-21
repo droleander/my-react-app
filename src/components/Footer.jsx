da47bf9e7e9608f48d6b3dfd3fea36d1288a7825
droleander <dro.github@outlook.com>
2023-11-21 21:20:37 +0800

    Updated

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -6,7 +6,7 @@ function Footer() {
   return (
     <footer className="container-fluid text-center">
       <div className="py-3 my-4 border-top">
-        <p className="m-0 text-body-secondary">&copy; {new Date().getFullYear()} website made with ☕ 💛 <img src={reactLogo} alt="React logo" /> <img src={viteLogo} alt="Vite logo" /> <img src={bootstrapLogo} alt="Bootstrap logo" /></p>
+        <p className="m-0 text-body-secondary">&copy; {new Date().getFullYear()} made with ☕ 💛 <img src={reactLogo} alt="React logo" /> <img src={viteLogo} alt="Vite logo" /> <img src={bootstrapLogo} alt="Bootstrap logo" /></p>
       </div>
     </footer>
   )
