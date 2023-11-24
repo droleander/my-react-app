a4d6d3fa2a15b5c31160549d21f09a37d1e50c77
droleander <dro.github@outlook.com>
2023-11-25 03:18:09 +0800

    Updated

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -6,13 +6,13 @@ function Card({persons, heading, handleDelete}) {
       <h2 className="text-center">{heading}</h2>
       <div className="container d-flex flex-wrap justify-content-center">
         {persons.map(person => (
-          <div className="card border-primary bg-light" key={person.idx}>
+          <div className="card border-primary bg-light" key={person.idx} title="Eh di ikaw na!">
             <img className="card-img-top" src={person.pic || "https://via.placeholder.com/150"} alt={`Photo - ${person.name}`} />
             <div className="card-body text-center">
               <h2 className="card-title">{person.name || "Name"}</h2>
               <h3 className="card-subtitle text-muted h6">{person.job || "Position"}</h3>
             </div>
-            <button className="btn btn-primary" onClick={() => handleDelete(person.idx)}><small>Eh di ikaw na!</small>{<br />}REMOVE</button>
+            <button className="btn btn-primary" onClick={() => handleDelete(person.idx)}>REMOVE EMPLOYEE</button>
           </div>
         ))}
       </div>
