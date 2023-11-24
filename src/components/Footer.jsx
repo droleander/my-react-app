57aa1b1874fbaa59ce7f95d175f7a90248440b05
droleander <dro.github@outlook.com>
2023-11-25 01:45:04 +0800

    Updated

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,10 +4,8 @@ import bootstrapLogo from '../assets/bootstrap.svg'
 
 function Footer() {
   return (
-    <footer className="container-fluid bg-light text-center">
-      <div className="py-3 border-top">
-        <p className="m-0 text-body-secondary">&copy; {new Date().getFullYear()} made with ☕ 💛 <img src={reactLogo} alt="React logo" /> <img src={viteLogo} alt="Vite logo" /> <img src={bootstrapLogo} alt="Bootstrap logo" /></p>
-      </div>
+    <footer className="container-fluid border-top bg-light py-3">
+      <p className="text-body-secondary text-center m-0">&copy; {new Date().getFullYear()} made with ☕ 💛 <img src={reactLogo} alt="React logo" /> <img src={viteLogo} alt="Vite logo" /> <img src={bootstrapLogo} alt="Bootstrap logo" /></p>
     </footer>
   )
 }
