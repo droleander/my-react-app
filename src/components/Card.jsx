b672f2b7c064f21ac6fb27d97ca4ddc9a0381fb3
droleander <dro.github@outlook.com>
2023-11-26 04:24:46 +0800

    Updated

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -12,7 +12,7 @@ function Card({employees, heading, handleDelete}) {
               <h2 className="card-title">{employee.name || "Name"}</h2>
               <h3 className="card-subtitle h6">{employee.job || "Position"}</h3>
             </div>
-            <button className="btn btn-warning py-2 mt-3" onClick={() => handleDelete(employee.idx)}>REMOVE EMPLOYEE</button>
+            <button className="btn btn-warning py-2 mt-3" onClick={() => handleDelete(employee.idx)}>REMOVE</button>
           </div>
         ))}
       </div>
