345fb2a3a24d27b897ecab561f034c200842ac5d
droleander <dro.github@outlook.com>
2023-11-25 23:39:28 +0800

    Updated

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -7,15 +7,12 @@ function Card({employees, heading, handleDelete}) {
       <div className="container d-flex flex-wrap justify-content-center">
         {employees.map(employee => (
           <div className="card bg-primary text-white m-2 p-4" key={employee.idx} title={`Eh di siya na ang ${employee.job}!`}>
-            <img className="card-img-top d-flex justify-content-center align-items-center border border-3 border-warning rounded-circle m-auto" src={employee.pic || "ttps://via.placeholder.com/150"} alt={`Photo - ${typeof employee.name !== "undefined" ? employee.name : "N.A."}`} />
+            <img className="card-img-top d-flex justify-content-center align-items-center border border-3 border-warning rounded-circle m-auto" src={employee.pic || "ttps://via.placeholder.com/150"} alt={`Photo - ${typeof employee.name !== "undefined" ? employee.name : "N.A."}`}/>
             <div className="card-body text-center">
               <h2 className="card-title">{employee.name || "Name"}</h2>
               <h3 className="card-subtitle h6">{employee.job || "Position"}</h3>
             </div>
-            <button className="btn btn-warning py-2 mt-3" onClick={() => {
-              alert(`${employee.name}... byeeee!`)
-              handleDelete(employee.idx)
-            }}>REMOVE EMPLOYEE</button>
+            <button className="btn btn-warning py-2 mt-3" onClick={() => handleDelete(employee.idx)}>REMOVE EMPLOYEE</button>
           </div>
         ))}
       </div>
