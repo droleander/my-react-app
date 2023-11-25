de33da05144e043c8f4f6b074a65545cb7db11ca
droleander <dro.github@outlook.com>
2023-11-25 22:18:34 +0800

    Updated

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -2,12 +2,12 @@
 function Card({employees, heading, handleDelete}) {
   
   return (
-    <main className="py-3">
+    <main className="flex-grow-1 flex-shrink-0 py-3">
       <h2 className="text-center">{heading}</h2>
       <div className="container d-flex flex-wrap justify-content-center">
         {employees.map(employee => (
-          <div className="card text-white bg-primary" key={employee.idx} title={`Eh di siya na ang ${employee.job}!`}>
-            <img className="card-img-top" src={employee.pic || "https://via.placeholder.com/150"} alt={`Photo - ${employee.name}`} />
+          <div className="card bg-primary text-white m-2 p-4" key={employee.idx} title={`Eh di siya na ang ${employee.job}!`}>
+            <img className="card-img-top d-flex justify-content-center align-items-center border border-3 border-warning rounded-circle m-auto" src={employee.pic || "ttps://via.placeholder.com/150"} alt={`Photo - ${typeof employee.name !== "undefined" ? employee.name : "N.A."}`} />
             <div className="card-body text-center">
               <h2 className="card-title">{employee.name || "Name"}</h2>
               <h3 className="card-subtitle h6">{employee.job || "Position"}</h3>
