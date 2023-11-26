1fc1dd3eb1158f553397b649f1219840eb0cc028
droleander <dro.github@outlook.com>
2023-11-26 21:42:39 +0800

    Updated

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -9,7 +9,7 @@ function Card({employees, heading, handleDelete}) {
           <div className="card bg-primary text-white m-2 p-4" key={employee.idx}>
             <img className="card-img-top d-flex justify-content-center align-items-center border border-3 border-warning rounded-circle m-auto" src={employee.pic} alt={`Photo - ${typeof employee.name !== "undefined" ? employee.name : "N.A."}`}/>
             <div className="card-body text-center">
-              <h2 className="card-title text-uppercase">{employee.name}</h2>
+              <h2 className="card-title text-uppercase h3">{employee.name}</h2>
               <p className="card-text mb-3 lh-1"><small>{employee.job}</small></p>
             </div>
             <button className="btn btn-warning py-2" onClick={() => handleDelete(employee.idx)}>REMOVE</button>
