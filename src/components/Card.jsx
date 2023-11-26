e878e3f1f930e871c41440e1296ad5fe6db963d7
droleander <dro.github@outlook.com>
2023-11-27 01:57:16 +0800

    Updated

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -6,13 +6,13 @@ function Card({employees, heading, handleDelete}) {
       <h2 className="text-center">{heading}</h2>
       <div className="container d-flex flex-wrap justify-content-center">
         {employees.map(employee => (
-          <div className="card bg-primary text-white m-2 p-4" key={employee.idx}>
+          <div className="card border-0 bg-primary rounded-5 text-white m-2 p-3" key={employee.idx}>
             <img className="card-img-top d-flex justify-content-center align-items-center border border-3 border-warning rounded-circle m-auto" src={employee.pic} alt={`Photo - ${typeof employee.name !== "undefined" ? employee.name : "N.A."}`}/>
             <div className="card-body text-center">
-              <h2 className="card-title text-uppercase h3">{employee.name}</h2>
-              <p className="card-text small mb-3">{employee.job}</p>
+              <h2 className="card-title border-bottom text-uppercase h3">{employee.name}</h2>
+              <p className="card-text lh-1">{employee.job}</p>
             </div>
-            <button className="btn btn-warning py-2" onClick={() => handleDelete(employee.idx)}>REMOVE</button>
+            <button className="btn btn-warning border-0 rounded-pill" onClick={() => handleDelete(employee.idx)}>REMOVE</button>
           </div>
         ))}
       </div>
