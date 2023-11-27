da2c86dcc389d8bba8bede339fb184f652679ae0
droleander <dro.github@outlook.com>
2023-11-27 11:11:31 +0800

    Updated

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -39,12 +39,10 @@ function Card({employees, heading, handleDelete}) {
                       <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                     </div>
                     <div className="modal-body">
-                      <p>
-                        Employee Name: {employee.name} <br />
-                        Position: {employee.job} <br />
-                        Employee ID: {employee.idx.slice(0, 8)} <br />
-                        Hobbies: {employee.hobbies}
-                      </p>
+                      <p>Employee Name: {employee.name}</p>
+                      <p>Position: {employee.job}</p>
+                      <p>Employee ID: {employee.idx.slice(0, 8)}</p>
+                      <p>Hobbies: {employee.hobbies}</p>
                     </div>
                     <div className="modal-footer">
                       <button type="button" className="btn btn-warning border-0 rounded-pill" data-bs-dismiss="modal">Close</button>
@@ -62,8 +60,7 @@ function Card({employees, heading, handleDelete}) {
               </button> */}
 
               {/* <!-- Button trigger removeModal --> */}
-              <button type="button" className="btn btn-warning border-0 rounded-pill mx-auto" data-bs-toggle="modal" data-bs-target={"#removeModal" + employee.idx}
-                onClick={() => handleDelete(employee.idx)}>
+              <button type="button" className="btn btn-warning border-0 rounded-pill mx-auto" data-bs-toggle="modal" data-bs-target={"#removeModal" + employee.idx} onClick={() => handleDelete(employee.idx)}>
                 Remove
               </button>
               {/* <!-- Modal --> */}
