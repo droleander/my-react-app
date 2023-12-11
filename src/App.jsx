2a49de0da7c4eb2c847154199c759382fd0ba0ab
droleander <dro.github@outlook.com>
2023-12-11 17:55:23 +0800

    Updated

--- src/App.jsx
+++ src/App.jsx
@@ -10,7 +10,6 @@ function App() {
     { idx: crypto.randomUUID(), name: "Luigi", job: "Assistant Plumber", pic: "https://www.dropbox.com/scl/fi/yysiljcwsg6663r4gfozu/SNS_Luigi.png?rlkey=x7tdpu6i4barco9yna7xgvia3&dl=1", hobbies: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Illo, sunt. In voluptate reprehenderit maxime incidunt." },
     { idx: crypto.randomUUID(), name: "Peach", job: "Princess", pic: "https://www.dropbox.com/scl/fi/bc3u4bjpkqi6szbnbptnw/SNS_Peach.jpg?rlkey=gllkz0t8b1w9ayyvthir24z1z&dl=1", hobbies: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsum fugit consequuntur cupiditate itaque ad deleniti!" },
     { idx: crypto.randomUUID(), name: "Bowser", job: "Antagonist", pic: "https://www.dropbox.com/scl/fi/njcdy15kq94yseez4pg3m/SNS_Bowser.png?rlkey=dx19vvgkum88w5savj0tog2f4&dl=1", hobbies: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Molestiae maxime, rem unde quos deleniti consequatur." },
-    { idx: crypto.randomUUID(), name: "", job: "", pic: "https://via.placeholder.com/150", hobbies: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Provident, tempora praesentium! Accusantium quibusdam iure dolores." }
     // { idx: crypto.randomUUID(), name: "Maureen", job: "CEO", pic: "https://www.dropbox.com/scl/fi/70x9rlpgi98z3n5790y8r/SNS_Maureen.jpg?rlkey=3bxfp0s554afdv40sqqep5tqn&dl=1", hobbies: "Making stamps, watching concerts, shopping online, etc." },
     // { idx: crypto.randomUUID(), name: "Cess", job: "Client Relationship Manager", pic: "https://www.dropbox.com/scl/fi/g1vxx0isfwueq6u08xz46/SNS_Ging.jpg?rlkey=siswahrmxh69u3t6keofpwxot&dl=1", hobbies: "Secret kasi SPG. LOL!" },
     // { idx: crypto.randomUUID(), name: "Tisoy", job: "IT Network Operations Analyst", pic: "https://www.dropbox.com/s/kv4tacxc5u92b9n/SNS_Picture.jpg?dl=1", hobbies: "Mag-internet, matulog, etc." },
@@ -24,6 +23,7 @@ function App() {
     // { idx: crypto.randomUUID(), name: "Rence", job: "Software Design Lead", pic: "https://www.dropbox.com/scl/fi/bmzgxaza72zk062xclbk5/SNS_Rence.jpg?rlkey=9a8c18s8aff2m0wwrdcl39q0x&dl=1", hobbies: "Playing video games, etc." },
     // { idx: crypto.randomUUID(), name: "Long", job: "IT Network Operations Lead", pic: "https://www.dropbox.com/scl/fi/uxxraly5k8su5kp9d5hpl/SNS_Long.jpg?rlkey=upnkzvjswdem0xv2qgnd0gdt2&dl=1", hobbies: "Kumain sa Aysee. LOL!" },
     // { idx: crypto.randomUUID(), name: "Aqui", job: "Asset Management Lead", pic: "https://www.dropbox.com/scl/fi/s7awq0du3wx4yr98wgqn1/SNS_Aqui.jpg?rlkey=m5agi094v54bdqbt8ic6nuxfa&dl=1", hobbies: "Biking, mamasyal kung saan-saan, etc." },
+    { idx: crypto.randomUUID(), name: "", job: "", pic: "https://via.placeholder.com/150", hobbies: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Provident, tempora praesentium! Accusantium quibusdam iure dolores." }
   ];
   
   const [employees, setEmployees] = useState(people);
@@ -36,7 +36,10 @@ function App() {
   return (
     <>
       <Header />
-      <Card employees={employees} handleDelete={handleDelete} heading="Current Employees" />
+      {employees.length !== 0 && 
+        <Card employees={employees} handleDelete={handleDelete} heading="Current Employees" /> ||
+        <main className="display-6 m-auto">No records found</main>
+      }
       <Footer />
     </>
   )
